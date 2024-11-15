85b7232b9a07d935dbcf169592971caf75c6c1cd
Timo Lehtola <timo.lehtola@eficode.com>
2024-11-15 13:29:54 +0200

    adjust message

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -4,7 +4,7 @@ const core = require("@actions/core");
 async function run() {
 	const joke = await getJoke();
 	console.log(joke);
-	core.setOutput("joke-output", joke);
+	core.setOutput("joke-output:", joke);
 }
 
 run();
